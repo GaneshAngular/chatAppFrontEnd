377a9aab77581e747adead020050c8b85d8b14a7
ganesh <ganeshturate1.com>
2025-01-17 14:56:41 +0530

    change server url

--- src/app/core/constants/Urls.ts
+++ src/app/core/constants/Urls.ts
@@ -1,22 +1,20 @@
+const SERVER_URL = 'https://chatappbackend-d53s.onrender.com';
 
-const SERVER_URL='http://localhost:8080'
+const USER_LOGIN = '/auth/login';
+const USER_SIGNUP = '/auth/signup';
+const VERIFY_OTP = '/auth/login/verify';
 
-const USER_LOGIN='/auth/login'
-const USER_SIGNUP='/auth/signup'
-const VERIFY_OTP='/auth/login/verify'
+const GET_USERS = '/users/';
+const GET_CONTACTS = '/users/contacts/';
+const SEND_MESSAGE = '/users/message/';
+const GET_MESSAGE = '/users/message/';
+const GET_PROFILE = '/users/self';
+const GET_USERS_BY_ID = '/users/id';
 
-const GET_USERS='/users/'
-const GET_CONTACTS='/users/contacts/'
-const SEND_MESSAGE='/users/message/'
-const GET_MESSAGE='/users/message/'
-const GET_PROFILE='/users/self'
-const GET_USERS_BY_ID='/users/id'
+const UPDATE_PROFILE_IMAGE = '/users/self/profile/';
+const UPDATE_PROFILE_DETAIL = '/users/self/detail';
 
-const UPDATE_PROFILE_IMAGE='/users/self/profile/'
-const UPDATE_PROFILE_DETAIL='/users/self/detail'
-
-export
-{
+export {
   USER_LOGIN,
   USER_SIGNUP,
   SERVER_URL,
@@ -28,5 +26,5 @@ export
   GET_PROFILE,
   GET_USERS_BY_ID,
   UPDATE_PROFILE_IMAGE,
-  UPDATE_PROFILE_DETAIL
-}
+  UPDATE_PROFILE_DETAIL,
+};
